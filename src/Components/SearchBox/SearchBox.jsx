07fd8e64eb372c3e8591d453d3f9b84780441fde
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-05-20 16:58:30 +0300

    add searchbox style

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -15,7 +15,7 @@ export const SearchBox = () => {
         <input
           className={s.searchbox__input}
           type="text"
-          placeholder="Enter note name"
+          placeholder="Search"
           name="name"
           onChange={handleSearch}
         />
